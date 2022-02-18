20aa23555bea8e694664cec5ba85d8a9668506b2
gureumwoon <vfrvfr3221@gmail.com>
2022-02-18 23:50:36 +0900

    js파일 생성

---
+++ carousel/index.js
@@ -0,0 +1,7 @@
+document.querySelector(".btn2").addEventListener('click', function () {
+    document.querySelector(".list").style.transform = 'translate(-30vw)';
+});
+
+document.querySelector(".btn3").addEventListener('click', function () {
+    document.querySelector(".list").style.transform = 'translate(-60vw)';
+});
