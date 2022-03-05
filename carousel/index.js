bb751d69f9aadb3609c2639017dd04f009262651
gureumwoon <vfrvfr3221@gmail.com>
2022-03-05 23:50:40 +0900

    slide list 코드 수정

--- carousel/index.js
+++ carousel/index.js
@@ -3,3 +3,18 @@ const nextBtn = document.querySelector('.next-btn');
 const list = document.querySelector('.list');
 const imgs = document.querySelector('.item');
 let currentIndex = 1;
+
+const size = list.clientWidth;
+console.log(size);
+list.style.transform = 'translateX(' + currentIndex * -size + 'px)';
+
+function imgTransition() {
+    console.log(imgs[currentIndex].id)
+    if (imgs[currentIndex].id === 'first-img') {
+        list.style.transition = 'none';
+        currentIndex = imgs.length - currentIndex;
+    } list.style.transform = 'translateX(' + currentIndex * -size + 'px)';
+}
+
+
+list.addEventListener('transitioned', imgTransition);
